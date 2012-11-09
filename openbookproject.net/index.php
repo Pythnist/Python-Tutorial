1115900af0e772b90c017ed903cc5978349f0ae8
Sourav Rockhound <souravgola@live.co.uk>
2012-11-09 17:36:29 +0530

    Python

---
+++ openbookproject.net/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Open Book Project</title>
+<base href="http://openbookproject.net/">
+<link rel="stylesheet" type="text/css" href="obp.css">
+</head>
+
+<body>
+<div id="Container">
+
+<div id="Header">
+  <div style="float: left; margin-bottom: 0px;">
+    <a href="index.php"><img src="images/OBPlogo.png" alt="OBP logo" /></a>
+  </div>
+  <div id="Title">
+  Open Book Project
+  <div class="Quote">
+    
+<p style="margin: 40px 50px 0px 100px;"><em>
+"Imagine a world in which every single person on the planet is given
+         free access to the sum of all human knowledge."
+</em></p>
+<p style="text-indent: 60%; margin-bottom: 0; font-style: normal;">
+-- Jimmy Wales,
+         Founder of Wikipedia</p>
+  </div>
+  </div>
+</div>
+
+<div id="Nav">
+  <div class="MenuItem"><a href="books">Books</a></div>
+  <div class="MenuItem"><a href="tutorials">Tutorials</a></div>
+  <div class="MenuItem"><a href="courses">Courses</a></div>
+  <div class="MenuItem"><a href="projects">Projects</a></div>
+  <div class="MenuItem"><a href="pybiblio">Python Bibliotheca</a></div>
+  <div class="MenuItem"><a href="multimedia">Multimedia</a></div>
+  <div class="MenuItem"><a href="mission.php">Mission</a></div>
+  <div class="MenuItem"><a href="contrib">Contributors</a></div>
+  <div class="MenuItem"><a href="index.php">Home</a></div>
+</div>
+
+<div id="Content">
+
+<h2>Mission</h2>
+<p>
+The Open Book Project is aimed at the educational community and seeks to
+encourage and coordinate collaboration among students and teachers for the
+development of high quality, freely distributable textbooks and educational
+materials on a wide range of topics.  The advent of the Internet and the World
+Wide Web are making collaboration among educators on a global scale possible
+for the first time.  We want to harness this exciting technology to promote
+learning and sharing.
+</p>
+
+<h2>What's New</h2>
+<ul>
+<li><a href="thinkcs">New Edition of How to Think Like a Computer Scientist:
+Learning with Python</a></li>
+<li><a href="py4fun">Python for Fun</a></li>
+<li>
+  <a href="courses/scratch">Creative Computer Exploration with Scratch</a>
+</li>
+<li><a href="pybiblio/gasp/course">GASP Python Course</a></li>
+<li><a href="books/socratic">Socratic Electronics</a></li>
+</ul>
+
+</div>
+
+<div id="Footer">
+<a href="http://www.ibiblio.org/">
+<img src="http://www.ibiblio.org/hosted/images/sm_hosted_trans.gif"
+width="100" height="30" alt="hosted by ibiblio" /></a>
+</div>
+
+<p style="text-align: center;">
+<a href="http://validator.w3.org/check/referer">Valid HTML</a> |
+<a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3">Valid
+CSS</a>
+</p>
+
+</div>
+
+</body>
+</html>
---
+++ openbookproject.net/mission.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Open Book Project</title>
+<base href="http://openbookproject.net/">
+<link rel="stylesheet" type="text/css" href="obp.css">
+</head>
+
+<body>
+<div id="Container">
+
+<div id="Header">
+  <div style="float: left; margin-bottom: 0px;">
+    <a href="index.php"><img src="images/OBPlogo.png" alt="OBP logo" /></a>
+  </div>
+  <div id="Title">
+  Open Book Project
+  <div class="Quote">
+    
+<p style="margin: 40px 50px 0px 100px;"><em>
+"A tree is known by its fruit; a man by his deeds. A good deed is never 
+         lost; he who sows courtesy reaps friendship, and he who plants
+         kindness gathers love."
+</em></p>
+<p style="text-indent: 60%; margin-bottom: 0; font-style: normal;">
+-- St. Basil</p>
+  </div>
+  </div>
+</div>
+
+<div id="Nav">
+  <div class="MenuItem"><a href="books">Books</a></div>
+  <div class="MenuItem"><a href="tutorials">Tutorials</a></div>
+  <div class="MenuItem"><a href="courses">Courses</a></div>
+  <div class="MenuItem"><a href="projects">Projects</a></div>
+  <div class="MenuItem"><a href="pybiblio">Python Bibliotheca</a></div>
+  <div class="MenuItem"><a href="multimedia">Multimedia</a></div>
+  <div class="MenuItem"><a href="mission.php">Mission</a></div>
+  <div class="MenuItem"><a href="contrib">Contributors</a></div>
+  <div class="MenuItem"><a href="index.php">Home</a></div>
+</div>
+
+<div id="Content">
+
+<p>
+The inspiration for this project comes largely from the efforts of Richard
+Stallman and the <a href="http://www.gnu.org">Free Software Foundation</a>. We
+are motivated to both promote learning and freedom, and to encourage the
+members of our community to become good neighbors.
+</p>
+
+<p>
+The Open Book Project is aimed at the educational community and seeks to
+encourage and coordinate collaboration among students and teachers for the
+development of high quality, freely distributable textbooks and educational
+materials on a wide range of topics.  The advent of the Internet and the World
+Wide Web are making collaboration among educators on a global scale possible
+for the first time. We want to harness this exciting technology to promote
+learning and sharing.  We will thus produce all of our materials with the
+following aims and guidelines in mind:
+</p>
+
+<ol>
+  <li>All educational materials will be covered by a free license such as
+  <a href="http://www.gnu.org/copyleft/copyleft.html">Copyleft</a>
+  to insure contributors that their contributions will remain freely available
+  for all users to share and develop.</li>
+
+  <li>All software used in our textbooks will be covered by the 
+  <a href="http://www.gnu.org/copyleft/gpl.html">GNU General Public License</a> 
+  or some similar free software license.</li>
+
+  <li>We will promote the development of a community of learners by actively
+  encouraging both teachers and students to take part in the development of
+  educational materials.</li>
+
+  <li>We will support the equality of languages by helping to make our
+  materials available in as many languages as our resources permit.  We will 
+  be developing materials in both English and Spanish ourselves, and we will
+  seek out and host translations into other languages.</li>
+
+  <li>We will make extensive use of web based technologies to make our
+  projects multidimensional and interactive.  We will keep explainations
+  as concise as possible and use hands on, "learn by doing" methods wherever
+  we can.</li>
+</ol>
+
+</div>
+
+<div id="Footer">
+<a href="http://www.ibiblio.org/">
+<img src="http://www.ibiblio.org/hosted/images/sm_hosted_trans.gif"
+width="100" height="30" alt="hosted by ibiblio" /></a>
+</div>
+
+<p style="text-align: center;">
+<a href="http://validator.w3.org/check/referer">Valid HTML</a> |
+<a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3">Valid
+CSS</a>
+</p>
+
+</div>
+
+</body>
+</html>
